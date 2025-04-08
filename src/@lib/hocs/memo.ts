d9dff2341d7bc929580d51c3687a4fce4cb3aa85
alchogh <alchogh01@gmail.com>
2025-04-09 03:08:36 +0900

    add : 심화과제 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement, ReactElement, useRef } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  return function MemorizedComponent(props: P): ReactElement {
+    const prevPropsRef = useRef<null | P>(null);
+    const prevRenderRef = useRef<ReactElement | null>(null);
+
+    const isUpdated =
+      !prevPropsRef.current || !_equals(prevPropsRef.current, props);
+
+    if (isUpdated) {
+      prevPropsRef.current = props;
+      prevRenderRef.current = createElement(Component, props);
+    }
+
+    return prevRenderRef.current!;
+  };
 }
